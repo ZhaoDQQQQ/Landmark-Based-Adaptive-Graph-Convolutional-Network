7768e8f84f15b429a08fb2d39a4fd8c6c55aeca2
DaQi Zhao <48011586+ZhaoDQQQQ@users.noreply.github.com>
2024-10-14 22:14:15 +0800

    Add files via upload

---
+++ configs/ck+/time_seg_16/face_flow_4_j_b_jm_bm.py
@@ -0,0 +1,121 @@
+multiflow = dict(
+    type='RecognizerGCNMultiflow',
+    which_flow=["j", "b", "jm", "bm"],
+    flow_weight={"j": 1.0, "b": 1.0, "jm": 0.5, "bm": 0.5},
+    num_classes=7,
+)
+model_j = dict(
+    type='RecognizerGCNSingleFlow',
+    backbone=dict(
+        type='STGCN',
+        gcn_adaptive='init',
+        gcn_with_res=True,
+        tcn_type='mstcn',
+        graph_cfg=dict(layout='face68', mode='spatial')),
+    cls_head=dict(
+        type='GCNHead',
+        num_classes=7,
+        in_channels=256,
+        loss_cls=dict(type='CrossEntropyLoss', loss_weight=multiflow["flow_weight"]["j"])),
+    softmax=True)
+
+model_b = dict(
+    type='RecognizerGCNSingleFlow',
+    backbone=dict(
+        type='STGCN',
+        gcn_adaptive='init',
+        gcn_with_res=True,
+        tcn_type='mstcn',
+        graph_cfg=dict(layout='face68', mode='spatial')),
+    cls_head=dict(
+        type='GCNHead',
+        num_classes=7,
+        in_channels=256,
+        loss_cls=dict(type='CrossEntropyLoss', loss_weight=multiflow["flow_weight"]["b"])),
+    softmax=True)
+
+model_jm = dict(
+    type='RecognizerGCNSingleFlow',
+    backbone=dict(
+        type='STGCN',
+        gcn_adaptive='init',
+        gcn_with_res=True,
+        tcn_type='mstcn',
+        graph_cfg=dict(layout='face68', mode='spatial')),
+    cls_head=dict(
+        type='GCNHead',
+        num_classes=7,
+        in_channels=256,
+        loss_cls=dict(type='CrossEntropyLoss', loss_weight=multiflow["flow_weight"]["jm"])),
+    softmax=True)
+
+model_bm = dict(
+    type='RecognizerGCNSingleFlow',
+    backbone=dict(
+        type='STGCN',
+        gcn_adaptive='init',
+        gcn_with_res=True,
+        tcn_type='mstcn',
+        graph_cfg=dict(layout='face68', mode='spatial')),
+    cls_head=dict(
+        type='GCNHead',
+        num_classes=7,
+        in_channels=256,
+        loss_cls=dict(type='CrossEntropyLoss', loss_weight=multiflow["flow_weight"]["bm"])),
+    softmax=True)
+
+dataset_type = 'PoseDataset'
+
+ann_file = r"..\data\ck+_10_fold\ck_landmarks_0.pkl"
+train_pipeline = [
+    dict(type='PreNormalize2D'),
+    dict(type='GenSkeFeat', dataset='face68', feats=["j", "b", "jm", "bm"]),
+    dict(type='UniformSample', clip_len=32),
+    dict(type='PoseDecode'),
+    dict(type='FormatGCNInput', num_person=1),
+    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['keypoint'])
+]
+val_pipeline = [
+    dict(type='PreNormalize2D'),
+    dict(type='GenSkeFeat', dataset='face68', feats=["j", "b", "jm", "bm"]),
+    dict(type='UniformSample', clip_len=32, num_clips=2, test_mode=True),
+    dict(type='PoseDecode'),
+    dict(type='FormatGCNInput', num_person=1),
+    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['keypoint'])
+]
+test_pipeline = [
+    dict(type='PreNormalize2D'),
+    dict(type='GenSkeFeat', dataset='face68', feats=["j", "b", "jm", "bm"]),
+    dict(type='UniformSample', clip_len=32, num_clips=2, test_mode=True),
+    dict(type='PoseDecode'),
+    dict(type='FormatGCNInput', num_person=1),
+    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['keypoint'])
+]
+data = dict(
+    videos_per_gpu=16,
+    workers_per_gpu=2,
+    test_dataloader=dict(videos_per_gpu=1),
+    train=dict(
+        type='RepeatDataset',
+        times=5,
+        dataset=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='train')),
+    val=dict(type=dataset_type, ann_file=ann_file, pipeline=val_pipeline, split='val'),
+    test=dict(type=dataset_type, ann_file=ann_file, pipeline=test_pipeline, split='val'))
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov=True)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
+total_epochs = 100
+checkpoint_config = dict(interval=10)
+evaluation = dict(interval=1, metrics=['top_k_accuracy'])
+log_config = dict(interval=80, hooks=[dict(type='TextLoggerHook')])
+
+# runtime settings
+log_level = 'INFO'
+
+work_dir = r'..\work_dirs\ck+_10_fold_multiflow_32_segment\ck_landmarks_0\j_b_jm_bm'
---
+++ configs/ck+/time_seg_16/face_j.py
@@ -0,0 +1,65 @@
+model = dict(
+    type='RecognizerGCN',
+    backbone=dict(
+        type='STGCN',
+        gcn_adaptive='init',
+        gcn_with_res=True,
+        tcn_type='mstcn',
+        graph_cfg=dict(layout='face68', mode='spatial')),
+    cls_head=dict(type='GCNHead', num_classes=7, in_channels=256))
+
+dataset_type = 'PoseDataset'
+
+ann_file = r"..\data\ck+_10_fold\ck_landmarks_0.pkl"
+train_pipeline = [
+    dict(type='PreNormalize2D'),
+    dict(type='GenSkeFeat', dataset='face68', feats=['j']),
+    dict(type='UniformSample', clip_len=32),
+    dict(type='PoseDecode'),
+    dict(type='FormatGCNInput', num_person=1),
+    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['keypoint'])
+]
+val_pipeline = [
+    dict(type='PreNormalize2D'),
+    dict(type='GenSkeFeat', dataset='face68', feats=['j']),
+    dict(type='UniformSample', clip_len=32, num_clips=2, test_mode=True),
+    dict(type='PoseDecode'),
+    dict(type='FormatGCNInput', num_person=1),
+    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['keypoint'])
+]
+test_pipeline = [
+    dict(type='PreNormalize2D'),
+    dict(type='GenSkeFeat', dataset='face68', feats=['j']),
+    dict(type='UniformSample', clip_len=32, num_clips=2, test_mode=True),
+    dict(type='PoseDecode'),
+    dict(type='FormatGCNInput', num_person=1),
+    dict(type='Collect', keys=['keypoint', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['keypoint'])
+]
+data = dict(
+    videos_per_gpu=16,
+    workers_per_gpu=2,
+    test_dataloader=dict(videos_per_gpu=1),
+    train=dict(
+        type='RepeatDataset',
+        times=5,
+        dataset=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='train')),
+    val=dict(type=dataset_type, ann_file=ann_file, pipeline=val_pipeline, split='val'),
+    test=dict(type=dataset_type, ann_file=ann_file, pipeline=test_pipeline, split='val'))
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov=True)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
+total_epochs = 100
+checkpoint_config = dict(interval=10)
+evaluation = dict(interval=1, metrics=['top_k_accuracy'])
+log_config = dict(interval=100, hooks=[dict(type='TextLoggerHook')])
+
+# runtime settings
+log_level = 'INFO'
+
+work_dir = r'..\work_dirs\ck+_10_fold_32_segment\ck_landmarks_0\j'
